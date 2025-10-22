24cd5ad10c829efdbd539908469a5585f51c3438
Jibran <mac@MacBook-Pro-2.lan>
2025-10-22 15:41:37 +0700

    feat: update pointer & work point

--- src/utils/Icons.tsx
+++ src/utils/Icons.tsx
@@ -1,4 +1,4 @@
-export { BiLink as LinkIcon } from "react-icons/bi";
+export { BiLink as LinkIcon, BiMenu as MenuIcon } from "react-icons/bi";
 export {
     FaLinkedin as LinkedInIcon,
     FaGithub as GitHubIcon,
@@ -6,7 +6,6 @@ export {
     FaInstagramSquare as InstagramIcon,
     FaYoutubeSquare as YoutubeIcon,
     FaVolumeUp as VolumeIcon,
-    FaHamburger as MenuIcon,
 } from "react-icons/fa";
 export { FiChevronDown as ChevronDownIcon, FiChevronUp as ChevronUpIcon } from "react-icons/fi";
 export { HiArrowSmRight as ArrowRightIcon, HiMail as MailIcon } from "react-icons/hi";
